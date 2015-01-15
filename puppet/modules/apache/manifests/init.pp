4e1f6d327fa375713e75647859196bd99e23d5dc
Stephen Holsapple <sholsapp@gmail.com>
2015-01-14 20:33:59 -0800

    Formatting

--- puppet/modules/apache/manifests/init.pp
+++ puppet/modules/apache/manifests/init.pp
@@ -6,19 +6,23 @@ class apache {
     'apache2-data',
     'apache2-utils',
   ]
-  package {$packages:
+
+  package { $packages:
     ensure => installed,
     before => File['/etc/apache2/apache2.conf'],
   }
-  file {'/etc/apache2/apache2.conf':
+
+  file { '/etc/apache2/apache2.conf':
     ensure  => file,
     owner   => 'root',
-    source => 'puppet:///modules/apache/apache2.conf',
+    source  => 'puppet:///modules/apache/apache2.conf',
   }
-  service {'apache2':
+
+  service { 'apache2':
     ensure    => running,
     enable    => true,
     subscribe => File['/etc/apache2/apache2.conf'],
   }
+
 }
 
